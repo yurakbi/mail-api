071fd8fe2428c1dd5d36ca1e0c56181102170281
Yura <yura04091@gmail.com>
2025-07-06 19:26:36 +0300

    send change

--- api/send-order.js
+++ api/send-order.js
@@ -1,6 +1,13 @@
 import nodemailer from "nodemailer";
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method not allowed" });
   }
@@ -26,4 +33,4 @@ export default async function handler(req, res) {
   } catch (error) {
     res.status(500).json({ message: "Помилка при відправці листа" });
   }
-} 
+}
